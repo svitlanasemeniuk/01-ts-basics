bdbde1a1968c353ad5cbc4a4cc1c7937a591feda
git@github.com:svitlanasemeniuk/goit-js-hw-10.git <svitlana.semeniuk11@gmail>
2026-08-30 19:02:44 +0200

    fix the task

--- src/task-8.ts
+++ src/task-8.ts
@@ -5,13 +5,13 @@ interface Post {
     title: string;
     body: string;
 }
-async function fetchPosts() {
+
+async function fetchPosts(): Promise<Post[]> {
     const response = await axios.get<Post[]>(
     'https://jsonplaceholder.typicode.com/posts'
     );
     return response.data;
 }
-
 fetchPosts().then((posts) => {
     console.log(posts[0].title);
 });
